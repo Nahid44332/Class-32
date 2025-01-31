ac0c08986b66c6b75892ae76d4163da12d29705e
Nahid44332 <nahidhossen3278@gmail.com>
2025-01-30 19:47:25 -0800

    Class-32

---
+++ scirpt.js
@@ -0,0 +1,38 @@
+let x = 70;
+var y = 60;
+
+
+let amount;
+
+var number;
+
+const chocolate = 120;
+
+
+
+console.log('Hello everyone');
+
+amount =90;
+number = 3;
+
+
+amount = 120;
+number = 2;
+
+
+let totalMoney = 500;
+let egg = 4;
+let kitkat = 2;
+let eachEgg = 15;
+let eachKitkat = 50;
+
+let eggPrice = egg*eachEgg;
+let kitkatPrice = kitkat*eachKitkat;
+
+let totalPrice = eggPrice+kitkatPrice;
+
+let remainingMoney = totalMoney-totalPrice;
+
+console.log(remainingMoney);
+
+
